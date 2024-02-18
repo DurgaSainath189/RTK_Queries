96de6d3ec8d3a9a62664bf52af4bba4a8d4b5138
Durga Sainath Dora <durgasainathdora@gmail.com>
2024-02-18 18:04:25 +0530

    added get all products query

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,11 +1,21 @@
-
+import { useGetAllProductsQuery } from "../app/service/dummyData";
 
 const AllProducts = () => {
+  const { data, isError, isLoading } = useGetAllProductsQuery();
+  if (isLoading) {
+    return <h1>Loading....</h1>;
+  }
+  if (isError) {
+    return <h1>Oops got an error</h1>;
+  }
+
   return (
     <div>
-      All Products
+      {data?.products.map((p) => (
+        <h1 key={p.id}>{p.title}</h1>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default AllProducts
+export default AllProducts;
